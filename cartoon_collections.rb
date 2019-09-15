6b0dd119d3d6047b0bb234de6c36df61c3b9c4dd
Jonah Brown <jswish02@gmail.com>
2019-09-15 05:45:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -53,7 +53,7 @@ def find_the_cheese2(array, array2)
   chesse_types = array2
   array.find do |type|
     chesse_types.include? (type)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
 end
 end
 find_the_cheese2(array,chesse_types)
